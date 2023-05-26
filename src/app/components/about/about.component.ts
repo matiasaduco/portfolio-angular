17adacc7ee72013fca5a95c74c424e521d0f19d1
Matias Aduco <matias.aduco@gmail.com>
2023-05-26 15:33:03 -0300

    Implementada la sección de About. Queda agregar la imagen y retocar el CSS para Hobbies y Aptitudes.

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css'],
+})
+export class AboutComponent {
+  // skills: any[] = [
+  //   { title: 'Angular', src: '../../../assets/icons/skills/angular.svg' },
+  //   { title: 'React', src: '../../../assets/icons/skills/react.svg' },
+  //   { title: 'HTML', src: '../../../assets/icons/skills/html-5.png' },
+  //   { title: 'CSS', src: '../../../assets/icons/skills/css-3.png' },
+  //   { title: 'JavaScript', src: '../../../assets/icons/skills/javascript.png' },
+  //   { title: 'TypeScript', src: '../../../assets/icons/skills/typescript.svg' },
+  // ];
+}
