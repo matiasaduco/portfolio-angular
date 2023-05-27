2d299b0309f78e6085135014a7413e944c165091
Matias Aduco <matias.aduco@gmail.com>
2023-05-27 18:24:50 -0300

    Agregué scroll completo entre secciones cuando se hace Scroll Up y Down.

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css'],
 })
 export class AboutComponent {
+  private lastScrollPosition: number = window.scrollY
+
   // skills: any[] = [
   //   { title: 'Angular', src: '../../../assets/icons/skills/angular.svg' },
   //   { title: 'React', src: '../../../assets/icons/skills/react.svg' },
@@ -14,4 +16,14 @@ export class AboutComponent {
   //   { title: 'JavaScript', src: '../../../assets/icons/skills/javascript.png' },
   //   { title: 'TypeScript', src: '../../../assets/icons/skills/typescript.svg' },
   // ];
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    const currentScroll = window.scrollY
+    if(currentScroll < this.lastScrollPosition) {
+      const section = document.getElementById('start')
+      section?.scrollIntoView()
+    }
+    this.lastScrollPosition = currentScroll
+  }
 }
--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-start',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start.component.css'],
 })
 export class StartComponent implements OnInit {
+  private lastScrollPosition: number = window.scrollY
+
   ngOnInit(): void {
     window.addEventListener('load', () => {
       const main = document.getElementById('start');
       main!.style.opacity = '1';
     });
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    const currentScroll = window.scrollY
+    if(currentScroll > this.lastScrollPosition) {
+      const section = document.getElementById('about')
+      section?.scrollIntoView()
+    }
+    this.lastScrollPosition = currentScroll
   }
 }
