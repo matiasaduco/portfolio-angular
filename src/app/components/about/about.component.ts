a83da33233ecc11c7bfcffca68ff546b76ed87d0
Matias Aduco <matias.aduco@gmail.com>
2023-05-28 02:49:09 -0300

    Completo el resto del push

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,24 +6,5 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./about.component.css'],
 })
 export class AboutComponent {
-  private lastScrollPosition: number = window.scrollY
 
-  // skills: any[] = [
-  //   { title: 'Angular', src: '../../../assets/icons/skills/angular.svg' },
-  //   { title: 'React', src: '../../../assets/icons/skills/react.svg' },
-  //   { title: 'HTML', src: '../../../assets/icons/skills/html-5.png' },
-  //   { title: 'CSS', src: '../../../assets/icons/skills/css-3.png' },
-  //   { title: 'JavaScript', src: '../../../assets/icons/skills/javascript.png' },
-  //   { title: 'TypeScript', src: '../../../assets/icons/skills/typescript.svg' },
-  // ];
-
-  @HostListener('window:scroll', ['$event'])
-  onScroll(event: Event) {
-    const currentScroll = window.scrollY
-    if(currentScroll < this.lastScrollPosition) {
-      const section = document.getElementById('start')
-      section?.scrollIntoView()
-    }
-    this.lastScrollPosition = currentScroll
-  }
 }
--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -6,22 +6,10 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./start.component.css'],
 })
 export class StartComponent implements OnInit {
-  private lastScrollPosition: number = window.scrollY
-
   ngOnInit(): void {
     window.addEventListener('load', () => {
       const main = document.getElementById('start');
       main!.style.opacity = '1';
     });
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  onScroll(event: Event) {
-    const currentScroll = window.scrollY
-    if(currentScroll > this.lastScrollPosition) {
-      const section = document.getElementById('about')
-      section?.scrollIntoView()
-    }
-    this.lastScrollPosition = currentScroll
   }
 }
